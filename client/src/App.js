f3937ce8df1375a388dfef2e64dbb40054fd9487
Soumya Adhya <soumyaadhya007@gmail.com>
2023-07-21 11:44:41 +0530

    added heading

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import Question from "./components/Question";
 function App() {
   return (
     <>
+      <h1 style={{ textAlign: "center", color: "#0c841b", fontSize: "40px" }}>
+        Meditation Baba
+      </h1>
+      <h3 style={{ textAlign: "center", color: "#17a354" }}>
+        Generates Meditation As Per Your Mood
+      </h3>
       <Question />
     </>
   );
